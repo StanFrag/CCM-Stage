5cf9394cff612bfe9fbaeb6115aaedc5b387c946
Vagrant User <vagrant@packer-virtualbox-iso-1422588891.vagrantup.com>
2015-09-07 15:07:37 +0200

    modif upload img

--- src/Application/Sonata/UserBundle/Import/UploadImg.php
+++ src/Application/Sonata/UserBundle/Import/UploadImg.php
@@ -62,24 +62,22 @@ class UploadImg {
         $file = $campaign->getImg();
 
         if (!$file instanceof UploadedFile) {
-            throw new \InvalidArgumentException(
-                'There is no file to upload!'
-            );
+            return;
+        }else{
+            $fileName = $this->generateFilename($file, $randomize);
+
+            $fs = new Filesystem();
+
+            try {
+                $fs->mkdir($this->directory, 0777);
+            } catch (IOExceptionInterface $e) {
+                echo "An error occurred while creating your directory at ".$e->getPath();
+            }
+
+            $file->move($this->directory, $fileName);
+
+            $campaign->setPath($fileName);
         }
-
-        $fileName = $this->generateFilename($file, $randomize);
-
-        $fs = new Filesystem();
-
-        try {
-            $fs->mkdir($this->directory, 0777);
-        } catch (IOExceptionInterface $e) {
-            echo "An error occurred while creating your directory at ".$e->getPath();
-        }
-
-        $file->move($this->directory, $fileName);
-
-        $campaign->setPath($fileName);
     }
 
     private function generateFilename($file, $randomize = true)
